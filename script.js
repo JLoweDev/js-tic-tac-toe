0db7621402adbb58cf670e671f7809ade97fe08b
James <67014970+JLoweDev@users.noreply.github.com>
2020-12-22 14:09:27 +0000

    Added event listeners for squares and reset button

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-//HTML Elements
+// HTML Elements
 const statusDiv = document.querySelector(".status");
 const resetDiv = document.querySelector(".reset");
 const cellDivs = document.querySelectorAll(".game-cell");
+
+// game variables
+let gameIsLive = true;
+let xIsNext = true;
+
+
+// event handlers
+const handleReset = (e) => {
+    console.log(e);
+}
+
+const handleCellClick = (e) => {
+    console.log(e);
+}
+
+
+// event listeners
+resetDiv.addEventListener('click', handleReset)
+
+for (const cellDiv of cellDivs) {
+    cellDiv.addEventListener('click', handleCellClick)
+}
+
+
+
+
 
 
 /*const gameBoard = {
