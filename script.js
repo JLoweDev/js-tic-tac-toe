edc06792ec39ccc545ab5315d79e365b291c69cf
James <67014970+JLoweDev@users.noreply.github.com>
2020-12-22 16:35:17 +0000

    Win logic added

--- script.js
+++ script.js
@@ -6,8 +6,15 @@ const cellDivs = document.querySelectorAll(".game-cell");
 // game variables
 let gameIsLive = true;
 let xIsNext = true;
+let winner = null;
 
 // functions
+const handleWin = (letter) => {
+    gameIsLive = false;
+    winner = letter;
+    statusDiv.innerHTML = `${winner} has won!`;
+}
+
 const checkGameStatus = () => {
     const topLeft = cellDivs[0].classList[2];
     const topMiddle = cellDivs[1].classList[2];
@@ -21,8 +28,24 @@ const checkGameStatus = () => {
 
     // check winner?
     if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+        handleWin(topLeft);
+    } else if (middleLeft && middleLeft === middleMiddle && middleLeft === middleRight) {
+        handleWin(middleLeft);
+    } else if (bottomLeft && bottomLeft === bottomMiddle && bottomLeft === bottomRight) {
+        handleWin(bottomLeft);
+    } else if (topLeft && topLeft === middleLeft && topLeft === bottomLeft) {
+        handleWin(topLeft);
+    } else if (topMiddle && topMiddle === middleMiddle && topMiddle === bottomMiddle) {
+        handleWin(topMiddle);
+    } else if (topRight && topRight === middleRight && topRight === bottomRight) {
+        handleWin(topRight);
+    } else if (topLeft && topLeft === middleMiddle && topLeft === bottomRight) {
+        handleWin(topLeft);
+    } else if (topRight && topRight === middleMiddle && topRight === bottomLeft) {
+        handleWin(topRight);
+    } else if (topLeft && topMiddle && topRight && middleLeft && middleMiddle && middleRight && bottomLeft && bottomMiddle && bottomRight) {
         gameIsLive = false;
-        statusDiv.innerHTML = `${topLeft} wins!`;
+        statusDiv.innerHTML = 'Draw';
     }
 };
 
