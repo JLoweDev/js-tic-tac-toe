4ac360f7d6a05e8642552a63822f099f4f6744e9
James <67014970+JLoweDev@users.noreply.github.com>
2020-12-22 16:19:36 +0000

    game functions basically

--- script.js
+++ script.js
@@ -7,6 +7,24 @@ const cellDivs = document.querySelectorAll(".game-cell");
 let gameIsLive = true;
 let xIsNext = true;
 
+// functions
+const checkGameStatus = () => {
+    const topLeft = cellDivs[0].classList[2];
+    const topMiddle = cellDivs[1].classList[2];
+    const topRight = cellDivs[2].classList[2];
+    const middleLeft = cellDivs[3].classList[2];
+    const middleMiddle = cellDivs[4].classList[2];
+    const middleRight = cellDivs[5].classList[2];
+    const bottomLeft = cellDivs[6].classList[2];
+    const bottomMiddle = cellDivs[7].classList[2];
+    const bottomRight = cellDivs[8].classList[2];
+
+    // check winner?
+    if (topLeft && topLeft === topMiddle && topLeft === topRight) {
+        gameIsLive = false;
+        statusDiv.innerHTML = `${topLeft} wins!`;
+    }
+};
 
 // event handlers
 const handleReset = (e) => {
@@ -14,7 +32,24 @@ const handleReset = (e) => {
 }
 
 const handleCellClick = (e) => {
-    console.log(e);
+    const classList = e.target.classList;
+    const location = classList[1];
+    
+    if (classList[2] === 'x' || classList[2] === 'o') {
+        return;
+    }
+    
+    if (xIsNext) {
+        classList.add('x');
+        statusDiv.innerHTML = "O is next";
+        checkGameStatus();
+        xIsNext = !xIsNext;
+    } else {
+        classList.add('o');
+        statusDiv.innerHTML = "X is next";
+        checkGameStatus();
+        xIsNext = !xIsNext;
+    }
 }
 
 
